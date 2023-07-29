77152576d438c14b8ae8631a20e2da977b342256
Abhishek-Epili <epiliabhishek@gmail.com>
2023-07-30 00:28:43 +0530

    adding product cards

---
+++ frontend/src/components/card.jsx
@@ -0,0 +1,11 @@
+function Card() {
+    return (
+        <div className="card product-card" style={{ width: "18rem" }}>
+            <img className="card-img-top" src="https://m.media-amazon.com/images/I/41LDfnScXZS._SX300_SY300_QL70_FMwebp_.jpg" alt="Card image cap" />
+            <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+    )
+}
+export default Card;
--- frontend/src/components/products.jsx
+++ frontend/src/components/products.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import Card from "./card";
 
 function ProdcutPage() {
 
@@ -23,71 +24,85 @@ function ProdcutPage() {
 
     return (
         <>
+            <br />
+            <h2>Products</h2>
             <div className="productpage">
-                <br />
-                <h2>Products</h2>
-                <div className="filters-container">
-                    <p>
-                        <a className="btn btn-primary filter-btn" data-bs-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Filter Products <i className="fa fa-caret-down" style={{ float: "right" }}></i></a>
-                    </p>
-                    <div className="row">
-                        <div className="col">
-                            <div className="collapse multi-collapse" id="multiCollapseExample1">
-                                <div className="card card-body">
-                                    <select value={filterType} onChange={handleFilterChange} style={{ width: "90%" }}>
-                                        <option value="" disabled hidden>Filter By Category</option>
-                                        <option value="Smart Phones">Smart Phones</option>
-                                        <option value="Laptops">Laptops</option>
-                                        <option value="Smart TVs">Smart TVs</option>
-                                    </select>
-                                    <br />
-                                    <br />
-                                    Select price filter:
-                                    <br />
-                                    <input type="radio" name="price_filter" className="filter-radio-btns" value="under" />Under
-                                    <input type="radio" name="price_filter" className="filter-radio-btns" value="above" />Above
-                                    <br />
-                                    <br />
-                                    <input type="number" value={price} onChange={handlePriceChange} placeholder="Enter amount" style={{ width: "90%" }} />
-                                    <br />
-                                    <br />
-                                    <button className="apply-btn">Apply</button>
-                                    <br />
-                                    <br />
+                <div className="filters-and-sort">
+
+
+                    <div className="filters-container">
+                        <p>
+                            <a className="btn btn-primary filter-btn" data-bs-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Filter Products <i className="fa fa-caret-down" style={{ float: "right" }}></i></a>
+                        </p>
+                        <div className="row">
+                            <div className="col">
+                                <div className="collapse multi-collapse" id="multiCollapseExample1">
+                                    <div className="card card-body">
+                                        <select value={filterType} onChange={handleFilterChange} style={{ width: "90%" }}>
+                                            <option value="" disabled hidden>Filter By Category</option>
+                                            <option value="Smart Phones">Smart Phones</option>
+                                            <option value="Laptops">Laptops</option>
+                                            <option value="Smart TVs">Smart TVs</option>
+                                        </select>
+                                        <br />
+                                        <br />
+                                        Select price filter:
+                                        <br />
+                                        <input type="radio" name="price_filter" className="filter-radio-btns" value="under" />Under&emsp;
+                                        <input type="radio" name="price_filter" className="filter-radio-btns" value="above" />Above
+                                        <br />
+                                        <br />
+                                        <input type="number" value={price} onChange={handlePriceChange} placeholder="Enter amount" style={{ width: "90%" }} />
+                                        <br />
+                                        <br />
+                                        <button className="apply-btn">Apply</button>
+                                        <br />
+                                        <br />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <br />
+                    <div className="sort-container">
+                        <p>
+                            <button className="btn btn-primary sort-btn" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Sort Products <i className="fa fa-caret-down" style={{ float: "right" }}></i></button>
+                        </p>
+                        <div className="row">
+                            <div className="col">
+                                <div className="collapse multi-collapse" id="multiCollapseExample2">
+                                    <div className="card card-body">
+                                        <select defaultValue={sortType} onChange={handleSortTypeChange} style={{ width: "90%" }}>
+                                            <option value="" disabled hidden>Sort By</option>
+                                            <option value="Price">Price</option>
+                                            <option value="Ratings">Ratings</option>
+                                            <option value="Newest">Newest</option>
+                                            <option value="Name">Name</option>
+                                        </select>
+                                        <br />
+                                        <br />
+                                        <input type="radio" name="sort_type" className="filter-radio-btns" value="asc" />Ascending&emsp;
+                                        <input type="radio" name="sort_type" className="filter-radio-btnsr" value="des" />Descending
+                                        <br />
+                                        <br />
+                                        <button className="apply-btn">Apply</button>
+                                        <br />
+                                        <br />
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <br />
-                <div className="sort-container">
-                    <p>
-                        <button className="btn btn-primary sort-btn" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Sort Products <i className="fa fa-caret-down" style={{ float: "right" }}></i></button>
-                    </p>
-                    <div className="row">
-                        <div className="col">
-                            <div className="collapse multi-collapse" id="multiCollapseExample2">
-                                <div className="card card-body">
-                                    <select defaultValue={sortType} onChange={handleSortTypeChange} style={{ width: "90%" }}>
-                                        <option value="" disabled hidden>Sort By</option>
-                                        <option value="Price">Price</option>
-                                        <option value="Ratings">Ratings</option>
-                                        <option value="Newest">Newest</option>
-                                        <option value="Name">Name</option>
-                                    </select>
-                                    <br />
-                                    <br />
-                                    <input type="radio" name="sort_type" className="filter-radio-btns" value="asc" />Ascending&emsp;
-                                    <input type="radio" name="sort_type" className="filter-radio-btnsr" value="des" />Descending
-                                    <br />
-                                    <br />
-                                    <button className="apply-btn">Apply</button>
-                                    <br />
-                                    <br />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                <div className="products">
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
                 </div>
             </div>
         </>
